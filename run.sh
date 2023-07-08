ffd2fbb570f4d69381435a70f97c77c467e5127d
Derek Smith <derek@farscapian.com>
2023-07-08 11:26:12 -0400

    Add run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
-set -ex
+set -exu
+
+CONNECTION_FILES_PATH=
+
+# grab any modifications from the command line.
+for i in "$@"; do
+    case $i in
+        --connection-csv-path=*)
+            CONNECTION_FILES_PATH="${i#*=}"
+            shift
+        ;;
+        *)
+        echo "Unexpected option: $1"
+        exit 1
+        ;;
+    esac
+done
+
+if [ -z "$CONNECTION_FILES_PATH" ]; then
+    echo "ERROR: you MUST specify the input path: --connection-csv-path="
+    exit 1
+fi
 
 docker pull node:latest
-
 IMAGE_NAME="roygbiv-load-tester:v0-0-1"
 docker build -t "$IMAGE_NAME" .
 
-CONNECTION_FILES_PATH="/home/farscapian/sovereign-stack/deployment/project/clams-server/output"
+for CSV_FILE in "$CONNECTION_FILES_PATH"/*.csv; do
+    echo "CSV_FILE: $CSV_FILE"
+    if [ -f "$CSV_FILE" ]; then
+        FILE_NAME=$(basename "$CSV_FILE")
 
-docker run -it -v "$CONNECTION_FILES_PATH":/connection_files "$IMAGE_NAME" /connection_files/cln_connection_info-roygbiv.team.csv
+        if ! docker ps | grep -q roygbiv-load-tester; then
+            docker run --name roygbiv-load-tester --rm -v  "$CONNECTION_FILES_PATH":/connection_files "$IMAGE_NAME" /connection_files/"$FILE_NAME"
+        fi
+
+        docker system prune -f
+
+        docker exec -it roygbiv-load-tester connection loadtest /connection_files/"$FILE_NAME"
+    fi
+done
